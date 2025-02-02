c2b5a9a1f82f90627f526999dfaf8801e59aa367
Aung Min Zan <nightkiller10@gmail.com>
2025-02-02 15:14:09 +0700

    add widgets: charts and overview

---
+++ app/Filament/Widgets/TreatmentsChart.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Treatment;
+use Filament\Widgets\ChartWidget;
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
+
+class TreatmentsChart extends ChartWidget
+{
+    protected static ?string $heading = 'Treatments';
+
+    protected function getData(): array
+    {
+        /**
+         * using flowframe/laravel-trend package to get the data
+         * for chart easily.
+         */
+        $data = Trend::model(Treatment::class)
+            ->between(
+                start: now()->subYear(),
+                end: now()
+            )
+            ->perMonth()
+            ->count();
+
+        return [
+            'datasets' => [
+                [
+                    'label' => 'Treatments',
+                    'data' => $data->map(fn (TrendValue $value) => $value->aggregate)
+                ]
+            ],
+            'labels' => $data->map(fn (TrendValue $value) => $value->date)
+        ];
+    }
+
+    protected function getType(): string
+    {
+        return 'line';
+    }
+}
